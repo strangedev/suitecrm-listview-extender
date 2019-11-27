cdc7050f9dd30c634b50b8aa8ed886666320581c
Noah Hummel <n.p.hummel@gmail.com>
2019-11-27 16:38:48 +0100

    FIX extend module-specific ViewList

--- templates.js
+++ templates.js
@@ -12,11 +12,13 @@ async function glob(ptrn) {
   });
 }
 
-async function getTemplates() {
-  return (await glob("plugin/**/*.mustache")).map(f => f.substr(7));
+async function getTemplates(multi = false) {
+  let ptrn = "plugin/**/*.mustache";
+  if (multi) ptrn += ".multi";
+  return (await glob(ptrn)).map(f => f.substr(7));
 }
 
-async function renderTemplates(jsonFile) {
+async function renderTemplates(jsonFile, multiRoot, multiKey) {
   const view = require(jsonFile);
   const files = await getTemplates();
   for (const f of files) {
@@ -25,8 +27,25 @@ async function renderTemplates(jsonFile) {
     console.info(`Rendering template ${f} -> ${name}`);
     await fs.writeFile(`build/sdist/${name}`, Mustache.render(template, view));
   }
+  const multis = await getTemplates(true);
+  for (const viewRoot of view[multiRoot]) {
+    for (const f of multis) {
+      const template = await fs.readFile(`plugin/${f}`, "utf8");
+      const name = `${path.basename(f, ".mustache.multi")}.${
+        viewRoot[multiKey]
+      }`;
+      console.info(`Rendering template ${f} -> ${name}`);
+      const tmpView = {};
+      tmpView[multiKey] = viewRoot[multiKey];
+
+      await fs.writeFile(
+        `build/sdist/${name}`,
+        Mustache.render(template, tmpView)
+      );
+    }
+  }
 }
 
-renderTemplates(process.argv[2])
+renderTemplates(process.argv[2], process.argv[3], process.argv[4])
   .then(() => {})
   .catch(console.error);
