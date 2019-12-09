d23f65fb895dcce8d1f1901c28ec36640746f87e
Noah Hummel <oss@noahhummel.consulting>
2019-12-09 17:40:10 +0100

    FIX add missing locale strings

--- plugin/locale/de_de.php
+++ plugin/locale/de_de.php
@@ -3,4 +3,4 @@
 $mod_strings['LBL_LV_DEBUGGER'] = 'Debugger';
 $mod_strings['LBL_LV_DEBUGGER_DESCRIPTION'] = 'Erkannte Hooks anzeigen';
 $mod_strings['LBL_LV_SECTION_HEADER'] = 'ListView Extender';
-$mod_strings['LBL_LV_SECTION_DESCRIPTION'] = '';
+$mod_strings['LBL_LV_SECTION_DESCRIPTION'] = 'Testen Sie die Konfiguration des ListView Extenders';
--- plugin/locale/en_us.php
+++ plugin/locale/en_us.php
@@ -3,4 +3,4 @@
 $mod_strings['LBL_LV_DEBUGGER'] = 'Debugger';
 $mod_strings['LBL_LV_DEBUGGER_DESCRIPTION'] = 'View registered hooks';
 $mod_strings['LBL_LV_SECTION_HEADER'] = 'ListView Extender';
-$mod_strings['LBL_LV_SECTION_DESCRIPTION'] = '';
+$mod_strings['LBL_LV_SECTION_DESCRIPTION'] = 'Test the ListView Extender configuration';
